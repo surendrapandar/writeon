c5ee1837cccaaf3704136adb20fbbc4bb7c6c35e
kashish sahu <kashishsahu03@gmail.com>
2024-10-23 11:24:33 +0530

    add the text

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex justify-center font-extrabold">
-      A tool to help you grow on linkedin without spending much time.
+      A tool to help you grow on linkedin without spending a lot of time.
     </div>
   );
 }
